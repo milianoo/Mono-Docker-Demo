3af02b81921cb35f18c96cc6beb47cf807464ee0
Milad Rk <milad@livedooh.com>
2016-05-16 17:19:16 +0800

    initial commit

---
+++ DockerDemoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DockerDemoApp
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World! This a Docker Demo app.");
+		}
+	}
+}
